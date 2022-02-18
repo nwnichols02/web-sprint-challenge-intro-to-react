4c60ba9e55e1f39c20d25c410d152a8cb34aaa7c
Nathan Nichols <nwnichols02@gmail.com>
2022-02-17 20:27:21 -0700

    functioning and rendering characters on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character';
 import './App.css';
+import axios from 'axios';
+
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+      axios.get('https://swapi.dev/api/people')
+        .then(res => {
+          console.log(res.data)
+          setCharacters(res.data);
+        })
+        .catch(err => {
+          console.error(err);
+        })
+  }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,8 +29,21 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((character, index) => {
+          return <Character key={index} info={character} ></Character>
+        })
+      }
     </div>
   );
-}
+  }
+  
+  export default App;
 
-export default App;
+//[GET] https://swapi.dev/api/people use to obtain characters
+
+//set list of characters to state useState(...)
+
+//name
+//birth_year
+//
+
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+
+export default function Character ({ info }) {
+    return (
+        <div>
+            <div>
+                <h3>{info.name}</h3>
+                <p>{info.birth_year}</p>
+                <p>Weight(LBS):{Math.round(info.mass * 2.2)}</p>
+            </div>
+        </div>
+    )
+}
